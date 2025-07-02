ee621e3b6d2d4fdd5e253ee2c829fd351b55742b
PawełAndrys <pawelandrys@gmail.com>
2025-07-02 10:11:04 +0200

    a

---
+++ stacks/otel-collector/aci-exporter.tf
@@ -0,0 +1,90 @@
+
+
+resource "kubernetes_config_map" "aci_exporter_config" {
+  metadata {
+    name      = "aci-exporter-config"
+    namespace = kubernetes_namespace.network.metadata[0].name
+  }
+  data = {
+    "aci-exporter.yaml" = <<-EOT
+apic:
+  url: "https://apic.example.com"
+  username: "admin"
+  password: "123"
+metrics:
+  - name: "fabricHealth"
+    class_name: "fabricHealthTotal"
+    labels: ["dn"]
+    values: ["cur","maxSev"]
+EOT
+  }
+}
+
+resource "kubernetes_deployment" "aci_exporter" {
+  metadata {
+    name      = "aci-exporter"
+    namespace = kubernetes_namespace.network.metadata[0].name
+    labels = {
+      app = "aci-exporter"
+    }
+  }
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        app = "aci-exporter"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "aci-exporter"
+        }
+      }
+      spec {
+        container {
+          name  = "aci-exporter"
+          image = "opsdis/aci-exporter:latest"
+          args = [
+            "--config.file=/config/aci-exporter.yaml"
+          ]
+          port {
+            container_port = 9300
+            name           = "http-metrics"
+            protocol       = "TCP"
+          }
+          volume_mount {
+            name       = "aci-exporter-config"
+            mount_path = "/config"
+            read_only  = true
+          }
+        }
+        volume {
+          name = "aci-exporter-config"
+          config_map {
+            name = kubernetes_config_map.aci_exporter_config.metadata[0].name
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "aci_exporter" {
+  metadata {
+    name      = "aci-exporter"
+    namespace = kubernetes_namespace.network.metadata[0].name
+  }
+  spec {
+    selector = {
+      app = "aci-exporter"
+    }
+    port {
+      name        = "http-metrics"
+      port        = 9300
+      target_port = 9300
+      protocol    = "TCP"
+    }
+    type = "ClusterIP"
+  }
+}
